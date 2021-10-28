653c56df1653baef080aaf047f6f6c97e056e49d
Yibaebi <elliot.yibaebi@yahoo.com>
2021-10-28 16:03:46 +0100

    add methods to get last chain block and create a new transaction.

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,7 +1,7 @@
 // Block chain constructor function
 function Blockchain() {
   this.chain = []
-  this.newTransactions = []
+  this.pendingTransactions = []
 }
 
 // Add create new block to block chain method
@@ -9,14 +9,31 @@ Blockchain.prototype.createNewBlock = function (nonce, hash, previousBlockHash) 
   const newBlock = {
     blockId: this.chain.length + 1,
     nonce,
-    transactions: this.newTransactions,
+    transactions: this.pendingTransactions,
     timestamp: Date.now(),
     hash,
     previousBlockHash,
   }
 
-  this.newTransactions = []
+  this.pendingTransactions = []
   this.chain.push(newBlock)
+}
+
+// Add function to get last block in chain
+Blockchain.prototype.getLastChainBlock = function () {
+  return this.chain[this.chain.length - 1]
+}
+
+// Create a new transaction
+Blockchain.prototype.createNewTransaction = function (amount, sender, receiver) {
+  const newTransaction = {
+    amount,
+    sender,
+    receiver,
+  }
+
+  this.pendingTransactions.push(newTransaction)
+  return this.getLastChainBlock()['blockId'] + 1
 }
 
 module.exports = Blockchain
--- dev/test.js
+++ dev/test.js
@@ -5,7 +5,14 @@ const bitcoin = new Blockchain()
 
 // Add a blocks to the existing chain
 bitcoin.createNewBlock(125667, 'h898weihouq2ihwekbnfjk', 'hwhsjew823jbjdsbjlafeokreniu')
-bitcoin.createNewBlock(12372, 'jsjhjskdhuwjkjskjsej', 'uudsiy892hjskhaohikfena')
-bitcoin.createNewBlock(12332, 'gshghsghsgsjskjkksh', 'yqw78728y892hjskhaohikksjfbjfena')
+bitcoin.createNewBlock(67281, 'jsjhjskdhuwjkjskjsej', 'uudsiy892hjskhaohikfena')
+bitcoin.createNewBlock(32, 'gshghsghsgsjskjkksh', 'yqw78728y892hjskhaohikksjfbjfena')
+bitcoin.createNewBlock(1627663, 'gsuywuyueywb63iuqnuiqh32uiu43bhs', '9j873yhqo02hqwhjeh')
 
-console.log('This is my bitcoin blockchain', bitcoin)
+// Get last block in chain
+bitcoin.getLastChainBlock()
+
+// Create a new transaction
+bitcoin.createNewTransaction(2000, 'Elliot', 'Joshua Nwokoye')
+
+console.log(bitcoin)
